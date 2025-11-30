6260f8f94ad0ce718673e02f98598dc49aa2156f
Nicolas gtrom <donosonicolas37@gmail.com>
2025-11-29 22:53:55 -0300

    Arreglando middleware sesiones

--- gym_system/settings.py
+++ gym_system/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'gym_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'gestion', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
